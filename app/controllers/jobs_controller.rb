c1be9ad319a88d28844bff484a94de1783a08b6d
Maxime Japsenne <japsenne@gmail.com>
2022-05-31 17:21:33 +0200

    def update-edit-destroy done

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,20 @@
 class JobsController < ApplicationController
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to jobs_path # Need to change the redirect
+
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path # Need to change the redirect
+  end
+
 end
