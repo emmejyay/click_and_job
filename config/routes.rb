0bcdb1108236beddf6eee64df0c691ff4ab52844
Maxime Japsenne <japsenne@gmail.com>
2022-06-02 15:42:43 +0200

    problem delete solved

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :jobs do
-    resources :requests, only: %i[new create edit update destroy]
-  end
+  get 'dashboard', to: "pages#dashboard", as: :dashboard
 
-  get '/dashboard', to: "pages#dashboard", as: :dashboard
+  resources :jobs do
+    resources :requests, only: %i[new create edit update]
+  end
+  resources :requests, only: :destroy
 end
