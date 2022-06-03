02b10e6bd05d6554aa64160621de830a44120eec
Maxime Japsenne <japsenne@gmail.com>
2022-06-03 12:06:18 +0200

    linked the index and show job in the dashboard of empoloyer

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'dashboard', to: "pages#dashboard", as: :dashboard
-  get 'jobs_created_by_user', to: "pages#dashboard", as: :jobs_created
+  get 'dashboard/:id', to: "pages#dashboard", as: :dashboard
+  get 'jobs_created_by_user', to: "pages#jobs_created_by_user", as: :jobs_created_by_user
 
   resources :jobs do
     resources :requests, only: %i[new create edit update]
