6aec735d1bb6d77072a5f83d520e643a75aeb54d
Maxime Japsenne <japsenne@gmail.com>
2022-06-07 11:29:45 +0200

    euh

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_083945) do
+ActiveRecord::Schema.define(version: 2022_06_07_080921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2022_06_06_083945) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "img_url"
     t.string "location"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
